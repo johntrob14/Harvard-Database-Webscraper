d82d727402d7fbed87ca62ce31e6bacb6cab359e
johntrob14 <77850850+johntrob14@users.noreply.github.com>
2022-10-14 16:26:56 -0500

    Nearly working webscraper, only parse_coding_sequence needs to be completed

--- main.py
+++ main.py
@@ -9,32 +9,69 @@ from bs4 import BeautifulSoup
 def write(s):
     with open('test.csv', 'w') as f:
         # create the csv writer
-        writer = csv.writer(f)
-
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerows(s)
         # write a row to the csv file
-        writer.writerow(s)
 
 
 # initialize an array of string values and labels for the csv data
 def main():
-    write(['PrimerBankID', 'GenBank Association', 'FPrimer', 'FPrimer length', 'FPrimer tm', 'FPrimer location',
-           'RPrimer', 'RPrimer length', 'FPrimer tm', 'RPrimer location', 'Coding Sequence', 'Validation Results'])
+    header = ['GenBank Accession', 'PrimerBankID', 'FPrimer', 'FPrimer length', 'FPrimer tm', 'FPrimer location',
+              'RPrimer', 'RPrimer length', 'FPrimer tm', 'RPrimer location', 'Coding Sequence', 'Validation Results']
     s = ["https://pga.mgh.harvard.edu/cgi-bin/primerbank/new_displayDetail2.cgi?primerID=6671509a1",
          "https://pga.mgh.harvard.edu/cgi-bin/primerbank/new_displayDetail2.cgi?primerID=6671509a2"]
+    big_array = [header]
     for x in s:
         # data = \
-        scrape(x)
-        # write(data)
+        data = scrape(x)
+        big_array.append(data)
+    write(big_array)
+
+
+def parse_coding_sequence(html_text, i):
+
+    # this is the final piece to finish, I need to parse the html file and make sure I only get the strings of nucleotides
+    # this should also remove all spaces in the strings so that they are one large continuous stream of nucleotide bases
 
 
 # scrape will eventually use beautiful soup to extract all text from the html and parse the html for a row of values.
 # the values will be returned as an array of strings which will be written by the write function
+
+
+
 def scrape(s):
     soup = BeautifulSoup(requests.get(s).content, 'html.parser')
-    all_html = soup.get_text()
+    all_html = soup.get_text().splitlines()
+    y = soup.get_text()
+    # print(all_html)
+    data = []
+    i = 0
     for x in all_html:
-        print(x)
+        if x == 'PrimerBank ID':
+            data.append(all_html[i + 1])
+        elif x == 'GenBank Accession':
+            data.append(all_html[i + 1])
+        elif x == 'Forward Primer':
+            data.append(all_html[i + 1])
+            data.append(all_html[i + 2])
+            data.append(all_html[i + 3])
+            data.append(all_html[i + 4])
+        elif x == 'Reverse Primer':
+            data.append(all_html[i + 1])
+            data.append(all_html[i + 2])
+            data.append(all_html[i + 3])
+            data.append(all_html[i + 4])
+        elif x == 'Location in Coding Sequence (primers and amplicon highlighted)':
+            x = parse_coding_sequence(all_html, i)
+            data.append(x)
+        i += 1
+
+    if 'Validation Results' in y:
+        data.append('YES')
+    else:
+        data.append('NO')
+
+    return data
 
 
 main()
-
